8d88c39f175a2db5d6703c078d7c88cb4755278e
jinwoo jung <alamash@naver.com>
2023-11-07 16:04:36 +0900

    [feat] 설정 UI 버튼 추가
    GoToMenu()를 AsyncLoadScene으로 구현해놓았는데 오류나면 지워도 무관

--- CityScape/Assets/Scripts/GameManager.cs
+++ CityScape/Assets/Scripts/GameManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.SceneManagement;
 using UnityEngine;
 using UnityEngine.Accessibility;
 using UnityEngine.SceneManagement;
@@ -7,9 +8,24 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance = null;
+    public static bool isMenu = true;
+    public static bool isGame = false;
+    public string roadMainMenu = "MainPopup";
+    [Header("MainUI")]
     [SerializeField] private GameObject settingPanel;
-    [SerializeField] private GameObject audioPanel;
+    [SerializeField] private GameObject offSettingPanelButton;
+    [SerializeField] private GameObject creditPanelButton;
+    [SerializeField] private GameObject exitButton;
+    [SerializeField] private GameObject settingText;
+
+    [Header("GameUI")]
+    [SerializeField] private GameObject PauseText;
+    [SerializeField] private GameObject continueButton;
+    [SerializeField] private GameObject restartButton;
+    [SerializeField] private GameObject goToMenuButton;
     
+    
+    [Header("Data")]
     [SerializeField] GameData gameData;
     [SerializeField] PlayerData playerData;
 
@@ -41,6 +57,42 @@ public class GameManager : MonoBehaviour
         if (!isGameover)
         {
             surviveTime += Time.deltaTime;
+        }
+    }
+    public void OnExitButton()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+    public void OnPausePanel()
+    {
+        settingPanel.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void OffPausePanel()
+    {
+        settingPanel.SetActive(false);
+        Time.timeScale = 1f;
+    }
+    public void GoMenuButton()
+    {
+        StartCoroutine(LoadMainMenuScene());
+    }
+    IEnumerator LoadMainMenuScene()
+    {
+        // AsyncOperation을 통해 Scene Load 정도를 알 수 있다.
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(roadMainMenu);
+
+        // Scene을 불러오는 것이 완료되면, AsyncOperation은 isDone 상태가 된다.
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+            isMenu = true;
+            isGame = false;
         }
     }
 }
