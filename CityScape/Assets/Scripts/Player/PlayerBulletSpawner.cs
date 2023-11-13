34f5ac2d25127c62316e42ebeaaf4785e2fb25a9
Jisu <92006284+jsomnium@users.noreply.github.com>
2023-11-14 01:10:29 +0900

    [FEAT] 플레이어 프리펩 안에 총알 스크립트 넣음

--- CityScape/Assets/Scripts/Player/PlayerBulletSpawner.cs
+++ CityScape/Assets/Scripts/Player/PlayerBulletSpawner.cs
@@ -4,28 +4,58 @@ using UnityEngine;
 
 public class PlayerBulletSpawner : MonoBehaviour
 {
-    public GameObject Player;
-    public int id;
-
-    public Vector3 FixPos; // 총알 생성 위치 조정
-
+    /* 총알 관련 */
     public float BulletSpawnTime;
+    public Vector3 FixPos;
 
+    /* 플레이어 관련 */
+    private GameObject Player;
+    public bool isPlayerLive;
+
+    /* 초기화 */
+    private void Awake()
+    {
+        Player = gameObject;
+    }
+
+    /* 플레이어가 생존 상태로 시작하고, 총알 생성 루틴을 실행함 */
     private void Start()
     {
+        isPlayerLive = true;
+
         StartCoroutine(SpawnRoutine());
+
+        // 레벨에 따른 총알 쿨타임 조정
         int asLevel = GameManager.instance.upgradeData.asLevel;
         BulletSpawnTime = BulletSpawnTime / (1.00f + (asLevel - 1) * 0.25f);
     }
 
+    /* 총알 생성 루틴 함수 */
     IEnumerator SpawnRoutine()
     {
+        yield return new WaitForSeconds(0.001f); // 에러 방지
+
         while (true)
         {
             GameObject obj = PoolManager.GetObject(0);
+            // Pool의 0번째 프리펩(총알)을 가져옴. 나중에 해당 부분 수정 가능성 있음
             obj.transform.position = Player.transform.position + FixPos;
-            yield return new WaitForSeconds(BulletSpawnTime); // BulletSpawnTime 초 만큼 대기 후 실행
             
+            yield return new WaitForSeconds(BulletSpawnTime); // BulletSpawnTime 초 만큼 대기 후 실행
+        }
+    }
+
+    /* 플레이어 사망 여부 체크 */
+    private void Update()
+    {
+        if (Player != null)
+        {
+            Player PlayerObj = Player.GetComponent<Player>();
+
+            if (PlayerObj != null)
+            {
+                isPlayerLive = PlayerObj.isLive; // Monster 오브젝트의 isLive 변수 가져오기
+            }
         }
     }
 }
