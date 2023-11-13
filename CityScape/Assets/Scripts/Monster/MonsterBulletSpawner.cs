537a87a7c6c7650014e4e73eb95fb8452a16e3a2
Jisu <92006284+jsomnium@users.noreply.github.com>
2023-11-14 00:45:45 +0900

    [FEAT] 몬스터 프리펩에서 총알 발사 구현
    - Monster Bullet Spawner 오브젝트 삭제
    - 스크립트를 몬스터 프리펩 안에 넣는 것으로 수정

--- CityScape/Assets/Scripts/Monster/MonsterBulletSpawner.cs
+++ CityScape/Assets/Scripts/Monster/MonsterBulletSpawner.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class MonsterBulletSpawner : MonoBehaviour
 {
     GameObject MyMonster;
+    GameObject TestMonster;
 
     public Vector3 FixPos; // 총알 생성 위치 조정
 
@@ -13,12 +14,19 @@ public class MonsterBulletSpawner : MonoBehaviour
 
     public bool isMonsterLive;
 
+    private void Awake()
+    {
+        MyMonster = gameObject;
+    }
 
     private void Start()
     {
         isMonsterLive = true; // Monster가 생존 상태로 시작
-        MyMonster = PoolManager.GetObject(2);
+
+        // TestMonster = PoolManager.GetObject(2); // 생성 코드
+
         StartCoroutine(SpawnRoutine());
+
     }
 
     IEnumerator SpawnRoutine() // 총알 생성 루틴
@@ -26,7 +34,7 @@ public class MonsterBulletSpawner : MonoBehaviour
         yield return new WaitForSeconds(WaitForApeear);
 
         while (isMonsterLive) // 몬스터 생존 경우만 총알 생성
-        { 
+        {
             GameObject MonsterBullet = PoolManager.GetObject(1);
             MonsterBullet.transform.position = MyMonster.transform.position + FixPos;
             yield return new WaitForSeconds(SpawnColltime); // BulletSpawnTime 초 만큼 대기 후 실행
@@ -44,4 +52,5 @@ public class MonsterBulletSpawner : MonoBehaviour
             }
         }
     }
+
 }
