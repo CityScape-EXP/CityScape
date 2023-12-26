271a388b94389a61b3808c20ec1822030f2a91d5
Jisu <92006284+jsomnium@users.noreply.github.com>
2023-12-26 16:00:29 +0900

    [ADD] 포션 구현

---
+++ CityScape/Assets/Scripts/Potion.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Potion : MonoBehaviour
+{
+    CircleCollider2D coll;
+    float moveSpeed = 5f;   // 배경 이동 속도와 동일
+    public int potionHP;    // 포션 회복량
+
+    private void Awake()
+    {
+        coll = GetComponent<CircleCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 배경 이동 속도와 동일하게 이동
+        transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+    }
+
+    // [포션, 플레이어] 충돌 시 비활성화
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            this.gameObject.SetActive(false);
+            Debug.Log("충돌 + " + gameObject);
+        }
+    }
+
+    // potionHP를 반환하는 함수
+    public int getPotionHP()
+    {
+        return potionHP;
+    }
+}
