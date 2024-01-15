a02406d0f0656fd097d9e401c99b46c855389661
jinwoo jung <alamash@naver.com>
2024-01-15 14:58:48 +0900

    주석추가

--- CityScape/Assets/Scripts/Ratio.cs
+++ CityScape/Assets/Scripts/Ratio.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Ratio : MonoBehaviour
+public class Ratio : MonoBehaviour  // 인게임에서 화면 비율에 따라 카메라의 위치를 바꿔서 해상도에 대응하는 스크립트
 {
     void Start()
     {
@@ -22,5 +22,5 @@ public class Ratio : MonoBehaviour
         }
         camera.rect = rect;
     }
-    void OnPreCull() => GL.Clear(true, true, Color.black);
+    void OnPreCull() => GL.Clear(true, true, Color.black); // 카메라가 비추는 이외의 공간을 검은색으로 칠함
 }
